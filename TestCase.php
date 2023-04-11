81668244913665080c1e63ee94213a768803373c
ankit10204 <therealrko.45@gmail.com>
2023-04-11 18:19:53 +0530

    Update TestCase.php

--- TestCase.php
+++ TestCase.php
@@ -10,7 +10,7 @@ class TestCase extends PhpUnitTest
 {
     public function testHello()
     {
-      echo getenv("API_KEY");
+      echo $_ENV["API_KEY"];
       $this->assertTrue(true);
     }  
 }
